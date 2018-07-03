88f7edd2347b095d7092323c6be130273c063e5e
Víctor Felipe Godoy Hernández <vigohe@gmail.com>
2018-07-02 22:42:41 -0400

    evolution

--- lib/elixir_pokedex.ex
+++ lib/elixir_pokedex.ex
@@ -4,6 +4,7 @@ defmodule ElixirPokedex do
 
     pokemons
     |> String.split(" ")
+    |> Enum.map(fn pokemon -> add_stat(pokemon) end)
   end
 
   def add_stat("charmander" = pokemon) do
@@ -36,5 +37,41 @@ defmodule ElixirPokedex do
       type: ["Normal"],
       evolution: nil
     }
+  end
+
+  def evolve(%{name: "charmander"}) do
+    %{
+      name: "Charmeleon",
+      type: ["Fire"],
+      evolution: "Charizard"
+    }
+  end
+
+  def evolve(%{name: name}) when name == "bulbasaur" do
+    %{
+      name: "Ivysaur",
+      type: ["Grass", "Poison"],
+      evolution: "Venusaur"
+    }
+  end
+
+  def evolve(%{name: name}) do
+    with {:ok, name} <- get_evolution(name) do
+      %{
+        name: name,
+        type: ["Water"],
+        evolution: "Blastoise"
+      }
+    else
+      {:error, message} -> message
+    end
+  end
+
+  defp get_evolution("squirtle") do
+    {:ok, "Wartortle"}
+  end
+
+  defp get_evolution(_) do
+    {:error, "Upssss! you don't have evolution..."}
   end
 end
