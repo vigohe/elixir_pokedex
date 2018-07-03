665659b96d56d405dce9051ed1b4539b0907ce8c
Víctor Felipe Godoy Hernández <vigohe@gmail.com>
2018-07-02 22:39:59 -0400

    add_stat

--- lib/elixir_pokedex.ex
+++ lib/elixir_pokedex.ex
@@ -5,4 +5,36 @@ defmodule ElixirPokedex do
     pokemons
     |> String.split(" ")
   end
+
+  def add_stat("charmander" = pokemon) do
+    %{
+      name: pokemon,
+      type: ["Fire"],
+      evolution: "Charmeleon"
+    }
+  end
+
+  def add_stat("bulbasaur" = pokemon) do
+    %{
+      name: pokemon,
+      type: ["Grass", "Poison"],
+      evolution: "Ivysaur"
+    }
+  end
+
+  def add_stat("squirtle") do
+    %{
+      name: "squirtle",
+      type: ["Water"],
+      evolution: "Wartortle"
+    }
+  end
+
+  def add_stat(_) do
+    %{
+      name: "Ditto",
+      type: ["Normal"],
+      evolution: nil
+    }
+  end
 end
